d0977c805fd61b5b42863e928d36d5a17f0fe9e0
Shahin developer <mdshahinbabu285@gmail.com>
2026-05-12 07:08:24 +0600

    Add proxy router to v1 API

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -2,10 +2,11 @@
 
 from fastapi import APIRouter
 
-from app.api.v1 import download, info, result, status
+from app.api.v1 import download, info, proxy, result, status
 
 router = APIRouter(prefix="/api/v1")
 router.include_router(download.router)
 router.include_router(status.router)
 router.include_router(result.router)
 router.include_router(info.router)
+router.include_router(proxy.router)
